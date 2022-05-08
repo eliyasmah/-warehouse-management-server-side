416dbb62f50d3bcaac7aea072e7a1ca91bb0750f
eliyasmah <eliyasbubt@gmail.com>
2022-05-09 03:23:14 +0600

    server connected with cline site

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config;
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xkfnh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("inventory management");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello from inventory-managing-server");
+  res.send("Hello from inventory-managing-server-site");
 });
 
 app.listen(port, () => {
